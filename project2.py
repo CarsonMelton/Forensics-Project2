82c451beaa6914731a8f756075039d93a2baeb51
Carson Melton <122568297+CarsonMelton@users.noreply.github.com>
2023-11-03 14:13:26 -0500

    Adding comments

--- project2.py
+++ project2.py
@@ -2,8 +2,7 @@ import os
 import sys
 import math
 
-# Need to add comments and edit the zip logic
-
+# Dictionary of File Headers
 file_header = {
     'MPG': '000001b3', 
     'PDF': '25504446', 
@@ -17,7 +16,7 @@ file_header = {
     'ZIP': '504b0304' 
     }
 
-
+# Dictionary of File Footers
 file_footer = {
     'MPG1': '000001b7',
     'MPG2': '000001b9', 
@@ -34,10 +33,16 @@ file_footer = {
 
 def FileRecovery(disk_hex):
     
+    # Number of files found
     total_found = 0
 
+    # Loop through each header
     for Header in file_header:
+
+        # Begin search at start of disk
         search = 0 
+
+        # Locations of header in image file
         loc = disk_hex.find(file_header[Header])
         while loc != -1:
 
@@ -48,13 +53,19 @@ def FileRecovery(disk_hex):
                     
                     total_found = total_found + 1
 
+                    # Find File Footer
                     footer = disk_hex.find(file_footer['MPG1'], loc)
+
                     if footer == -1: 
                         footer = disk_hex.find(file_footer['MPG2'], loc) 
+
+                    # Account for footer length
                     footer = footer + 7 
+
                     #Creating the file
                     File_name = 'File' + str(total_found) + '.mpg' 
                     
+                    # Calculate File Size
                     start_offset = int(loc / 2) 
                     end_offset = int(math.ceil(footer / 2))
                     file_size = end_offset - start_offset
@@ -69,6 +80,7 @@ def FileRecovery(disk_hex):
                     search = footer
 
                 else:
+                    # Move past current header to next available location
                     search = loc + 8
 
             elif Header == 'PDF':
@@ -76,6 +88,7 @@ def FileRecovery(disk_hex):
                     print()
                     total_found = total_found + 1
 
+                    # Check against all possible file footers
                     footer = disk_hex.find(file_footer['PDF1'], loc)
                     end_offset = 13 
 
@@ -92,11 +105,16 @@ def FileRecovery(disk_hex):
                         footer = disk_hex.find(file_footer['PDF4'], loc) 
                         end_offset = 11 
                     
+                    # Account for file offset
                     end_offset = end_offset + footer
+
                     File_name = 'File' + str(total_found) + '.pdf'
+
+                    # Calculate file size
                     start_offset = int(loc / 2)
                     end_offset = int(math.ceil(end_offset / 2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
@@ -106,13 +124,18 @@ def FileRecovery(disk_hex):
                     search = footer
 
                 else:
+                    # Move to space after current file header
                     search = loc + 8
 
             elif Header == 'BMP':
+
                 if (loc % 512) == 0 and disk_hex[(loc + 12):(loc + 20)] == '00000000':
+                        
                         total_found = total_found + 1
+                        
                         File_name = 'File' + str(total_found) + '.bmp'
 
+                        # Calculate File Size from header in little endian order
                         file_size = str(disk_hex[loc + 10: loc + 12])
                         file_size = file_size + str(disk_hex[loc + 8: loc + 10])
                         file_size = file_size + str(disk_hex[loc + 6: loc + 8])
@@ -121,6 +144,7 @@ def FileRecovery(disk_hex):
                         file_size = int(file_size, 16) 
                         start_offset = int(loc / 2)
                         end_offset = start_offset + file_size
+
                         print(File_name, end = ', ')
                         print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                         print('End Offset: ' + str(hex(end_offset)))
@@ -129,6 +153,7 @@ def FileRecovery(disk_hex):
                         
                         search = loc + file_size
                 else:
+                    # Move search location after current header
                     search = loc + 4
 
             elif Header == 'GIF1':
@@ -136,30 +161,48 @@ def FileRecovery(disk_hex):
                     
                     total_found = total_found + 1
 
+                    # Locate file footer
                     footer = disk_hex.find(file_footer['GIF'], loc)
+
+                    # Adjust for footer length
                     footer = footer + 3 
+
                     File_name = 'File' + str(total_found) + '.gif'
+
+                    # Calculate file size
                     start_offset = int(loc / 2)
                     end_offset = int(math.ceil(footer / 2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
+
                     File_Extract(start_offset,file_size, File_name)
 
                 else:
+                    # Move search location after current header
                     search = loc + 12
 
             elif Header == 'GIF2':
                 if (loc % 512) == 0:
                     print()
+
                     total_found = total_found + 1
+
+                    # Find File Footer
                     footer = disk_hex.find(file_footer['GIF'], loc)
+
+                    # Adjust for footer length
                     footer = footer + 3 
+
                     File_name = 'File' + str(total_found) + '.gif'
+
+                    # Calculate File Size
                     start_offset = int(loc / 2)
                     end_offset = int(math.ceil(footer / 2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
@@ -167,24 +210,29 @@ def FileRecovery(disk_hex):
                     File_Extract(start_offset,file_size, File_name)
 
                     search = footer
-
                 else:
+                    # Move search location after current header
                     search = loc + 12
 
             elif Header == 'JPG':
                 if (loc % 512) == 0:
                     print()
+
                     total_found = total_found + 1
 
-		   
+                    # Find File Footer
                     footer = disk_hex.find(file_footer['JPG'], loc)
+
+                    # Adjust for Footer Length
                     footer = footer + 3 
                     
                     File_name = 'File' + str(total_found) + '.jpg'
                     
+                    # Calculate File Size
                     start_offset = int(loc / 2)
                     end_offset = int(math.ceil(footer / 2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
@@ -193,20 +241,27 @@ def FileRecovery(disk_hex):
 
                     search = footer
                 else:
+                    # Move search location after current header
                     search = loc + 6
 
             elif Header == 'DOCX':
                 
                 if (loc % 512) == 0:
-                    total_found = total_found + 1        
+                    total_found = total_found + 1     
+
+                    # Find File Footer   
                     footer = disk_hex.find(file_footer['DOCX'], loc)
+
+                    # Adjust for footer length
                     footer = footer + 43 
 		            
                     File_name = 'File' + str(total_found) + '.docx'
                    
+                   # Calculate File Size
                     start_offset = int(loc / 2)
                     end_offset = int(math.ceil(footer / 2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
@@ -216,22 +271,29 @@ def FileRecovery(disk_hex):
                     search = footer
 
                 else:
+                    # Move search location after current header
                     search = loc + 16
 
             elif Header == 'AVI':
                 if (loc % 512) == 0:
+                    # Ensure full file header matches
                     if (disk_hex[(loc + 16):(loc + 32)] == '415649204c495354'):
+
                         total_found = total_found + 1
 
                         File_name = 'File' + str(total_found) + '.avi'
 
+                        # Calculate file size from file header
                         file_size = str(disk_hex[loc + 14: loc + 16])
                         file_size = file_size + str(disk_hex[loc + 12: loc + 14])
                         file_size = file_size + str(disk_hex[loc + 10: loc + 12])
                         file_size = file_size + str(disk_hex[loc + 8: loc + 10])
+
                         file_size = int(file_size, 16) + 8 
+
                         start_offset = int(loc / 2)
                         end_offset = start_offset + file_size
+
                         print(File_name, end = ', ')
                         print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                         print('End Offset: ' + str(hex(end_offset)))
@@ -240,17 +302,26 @@ def FileRecovery(disk_hex):
 
                         search = loc + file_size
                 else:
+                    # Move search location after current header
                     search = loc + 32
 
             elif Header == 'PNG':
                 if (loc % 512) == 0:
                     total_found = total_found + 1
+
+                    # Find File Footer
                     footer = disk_hex.find(file_footer['PNG'], loc)
+                    
+                    # Adjust for footer length 
                     footer = footer + 15  
+
                     File_name = 'File' + str(total_found) + '.png'
+
+                    # Calculate file size
                     start_offset = int(loc / 2)
                     end_offset = int(math.ceil(footer / 2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
@@ -259,6 +330,7 @@ def FileRecovery(disk_hex):
 
                     search = footer
                 else:
+                    # Move search location after current file header
                     search = loc + 16
             
             elif Header == "ZIP":
@@ -273,9 +345,11 @@ def FileRecovery(disk_hex):
                     end_offset = footer + len(file_footer['ZIP'])
                     File_name = 'File' + str(total_found) + '.zip'
 
+                    # Calculate file size 
                     start_offset = int(loc/2) + len(file_header['ZIP'])
                     end_offset = int(math.ceil(end_offset/2))
                     file_size = end_offset - start_offset
+
                     print(File_name, end = ', ')
                     print('Start Offset: ' + str(hex(start_offset)), end = ", ")
                     print('End Offset: ' + str(hex(end_offset)))
@@ -283,25 +357,31 @@ def FileRecovery(disk_hex):
                     File_Extract(start_offset,file_size, File_name)
                     search = footer*2
                 else:
+                    # Move search location to after current file header
                     search = loc + len(file_header['ZIP'])
 
                     
             loc = disk_hex.find(file_header[Header], search)
 
 def File_Extract(start_offset,count, file_name):
+    # Linux command to extract file data
     extraction_command = 'dd if=' + str(sys.argv[1]) + ' of=' + str(file_name) + ' bs=1 skip=' + str(start_offset) + ' count='+ str(count)
     os.system(extraction_command)
 
     generateHash(file_name)
 
 def generateHash(inputFile):
+    # Linux command to generate SHA256 sum for file 
     hash = 'sha256sum ' + inputFile
     os.system(hash)
 
 
 if __name__ == "__main__":
     input_disk_path = sys.argv[1]
+    # Read disk image
     with open(sys.argv[1], 'rb') as disk_image:
         disk_data = disk_image.read().hex()
     disk_image.close()
+
+    # Begin file recovery
     FileRecovery(disk_data)
